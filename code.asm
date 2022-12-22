f4f6f8593865309cad5b00e31cb36857e63ee524
Injy ElSherbini <90464702+mercury-i@users.noreply.github.com>
2022-12-22 11:55:18 +0200

    encryption and decryption fully functioning

--- code.asm
+++ code.asm
@@ -1,5 +1,4 @@
 .MODEL SMALL
-.STACK 100H
 
 
 .DATA
@@ -7,8 +6,10 @@
     FNAME DB 'output.txt',00H
     FHANDLE DW ?
     BUFFER DB ?
-    MSG1 DB 'ENTER KEY $'
+    MSG1 DB 'TYPE E FOR ENCRYPTING AND D FOR DECRYPTING $'
+    MSG2 DB 'ENTER KEY (1-9) $'
     KEY DB ?
+    OP DB ?
     
 .CODE
 
@@ -39,7 +40,34 @@
         RET
     CLOSE_FILE ENDP
     
-    ROT PROC NEAR
+    DROT PROC NEAR
+        CMP AL,65
+        JL NOTCHARACTER
+        CMP AL,122
+        JG NOTCHARACTER
+        CMP AL,90
+        JG LOWER
+        JMP UPPER
+        UPPER:
+        SUB AL,KEY
+        CMP AL,65
+        JL ADDALPHA
+        RET
+        LOWER:
+        CMP AL,97
+        JL NOTCHARACTER
+        SUB AL,KEY
+        CMP AL,97
+        JL ADDALPHA
+        RET
+        ADDALPHA:
+        ADD AL,26
+        RET
+        NOTCHARACTER:
+        RET
+    DROT ENDP
+    
+    EROT PROC NEAR
         CMP AL,65
         JL NOTCHAR
         CMP AL,122
@@ -48,26 +76,41 @@
         JG LOWERCASE
         JMP UPPERCASE
         UPPERCASE:
-        SUB AL,KEY
-        CMP AL,65
-        JL ADDALPHA
+        ADD AL,KEY
+        CMP AL,90
+        JG SUBALPHA
         RET
         LOWERCASE:
         CMP AL,97
         JL NOTCHAR
-        SUB AL,KEY
-        CMP AL,97
-        JL ADDALPHA
+        ADD AL,KEY
+        CMP AL,122
+        JG SUBALPHA
         RET
-        ADDALPHA:
-        ADD AL,26
+        SUBALPHA:
+        SUB AL,26
         RET
         NOTCHAR:
         RET
-        ROT ENDP
+    EROT ENDP
+    
+    ENCRYPT_OR_DECRYPT PROC NEAR
+        CMP OP,'D'
+        JE DECRYPT
+        CMP OP,'d'
+        JE DECRYPT
+        JMP ENCRYPT
+        DECRYPT:
+        CALL DROT
+        CALL SAVECHAR
+        RET
+        ENCRYPT:
+        CALL EROT
+        CALL SAVECHAR
+        RET
+    ENCRYPT_OR_DECRYPT ENDP
     
     SAVECHAR PROC NEAR
-        CALL ROT
         MOV BUFFER,AL
         MOV AH,40H
         MOV BX,FHANDLE
@@ -83,18 +126,32 @@
         INT 21H
         CMP AL,'@'
         JE EXIT
-        CALL SAVECHAR
+        CALL ENCRYPT_OR_DECRYPT
         JMP AGAIN
         EXIT:
         CALL CLOSE_FILE
         RET
     INPUT_TO_FILE ENDP
+    
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
     MAIN PROC FAR
         .STARTUP
+        
         MOV AH,09H
         LEA DX,MSG1
+        INT 21H
+        
+        MOV AH,01H
+        INT 21H
+        MOV OP,AL
+        
+        MOV AH,02H
+        MOV DL,0AH
+        INT 21H
+        
+        MOV AH,09H
+        LEA DX,MSG2
         INT 21H
         
         MOV AH,01H
@@ -102,15 +159,16 @@
         SUB AL,30H
         MOV KEY,AL
         
+        MOV AH,02H
+        MOV DL,0AH
+        INT 21H
+        
         CALL MAKE_NEW_FILE
         CALL INPUT_TO_FILE
         
-
         
         .EXIT
     MAIN ENDP
 END MAIN
 
 
-
-        
